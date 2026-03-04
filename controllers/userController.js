d10d4225b8d1ae5ae0c9758b159b77b78efa3e3a
abdelrahman-kamel-ahmed <kamelabdelrahman73@gmail.com>
2026-03-04 02:23:44 +0200

    feat: add Follow model with follower, followed, timestamps, and composite unique index followers and following count in the user model

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,13 @@ exports.getProfile = async (req, res) => res.status(501).json({ message: 'Not Im
 exports.updateProfile = async (req, res) => res.status(501).json({ message: 'Not Implemented' });
 
 // User Interaction Controllers
-exports.followUser = async (req, res) => res.status(501).json({ message: 'Not Implemented' });
+exports.followUser = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
 exports.unfollowUser = async (req, res) => res.status(501).json({ message: 'Not Implemented' });
 exports.getFollowers = async (req, res) => res.status(501).json({ message: 'Not Implemented' });
 exports.getFollowing = async (req, res) => res.status(501).json({ message: 'Not Implemented' });
